0dc658bd7d7565a467ec903716266c9512cc4ef5
syuhei176 <syuhei176@gmail.com>
2014-01-13 11:00:26 +0900

    アップデートスクリプト

--- update.sh
+++ update.sh
@@ -1,1 +1,10 @@
-wget 
+rm app.js
+rm config.js
+rm -r server
+rm -r public
+wget https://dl.dropboxusercontent.com/u/6931359/flower/app.js
+wget https://dl.dropboxusercontent.com/u/6931359/flower/server.zip
+wget https://dl.dropboxusercontent.com/u/6931359/flower/public.zip
+wget https://dl.dropboxusercontent.com/u/6931359/flower/config.js
+unzip server.zip
+unzip public.zip
