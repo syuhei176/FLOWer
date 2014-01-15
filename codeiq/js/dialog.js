4d2b76a62323b466b36e1cc5d618bc50d9813556
Nobukazu Hanada <tone.nobukazu@gmail.com>
2014-01-15 17:59:36 +0900

    fixed firefox view bugs

--- codeiq/js/dialog.js
+++ codeiq/js/dialog.js
@@ -13,6 +13,7 @@
 			"snapelement":["Translate","Fill",],
 			"jquery":["Find","Html",],
 			"system":["Print","Scan"],
+			"number" : ["0","1","2","3","4","5","6","7","8","9"],
 			"" : []};
 
 	var modalMemu = $("#joblist");
