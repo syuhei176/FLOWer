fd88f6daafab7625ee78d3cd40b41f9f9bc9683e
Nobukazu Hanada <tone.nobukazu@gmail.com>
2014-03-14 17:32:47 +0900

    IdGenerator fixed

--- src/retro/pub/IdGenerator.hx
+++ src/retro/pub/IdGenerator.hx
@@ -5,12 +5,10 @@ typedef EditorKey = String;
 
 class IDGenerator{
 	private var uniqueEditorKey : EditorKey;
-	private var counter : Int;
 	private static var idGen : IDGenerator; 
 
 	private function new(uniqueEditorKey){
 		this.uniqueEditorKey = uniqueEditorKey;
-		this.counter = 0;
 	}
 
 	public static function getInstance(uniqueEditorKey){
@@ -21,8 +19,7 @@ class IDGenerator{
 	}
 
 	public function genID(){
-		this.counter += 1;
-		return this.uniqueEditorKey + Std.string(this.counter);
+		return this.uniqueEditorKey + Std.string(Date.now().getTime());
 	}
 
 }
